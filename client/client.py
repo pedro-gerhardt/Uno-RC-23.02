025f32de74a761f198a9e949d0f6b498a73a5535
pedro-gerhardt <pedro.lucas.gerhardt@gmail.com>
2023-11-12 15:35:36 -0300

    cartas embaralhadas, sorteadas e enviadas a mao dos jogadores

--- client/client.py
+++ client/client.py
@@ -1,22 +1,37 @@
-import socket
+import socket, sys, pickle
+sys.path.append('../')
 from config import MSG_SIZE
+from msg import Msg
+from enums import TipoMsg
+from card import Carta
 
+mao = []
 
 def start():
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.connect((socket.gethostbyname(socket.gethostname()), 5050))
+    pronto = False
 
     while True:
-        msg = server_socket.recv(MSG_SIZE)
-        if msg.decode() == "Você está pronto?":
+        data = memoryview(bytearray(MSG_SIZE))
+        bc = server_socket.recv_into(data, MSG_SIZE)
+        if bc == 0:
+            continue
+        msg = pickle.loads(data)
+        print(msg.tipo)
+        if not pronto and msg.tipo == TipoMsg.PRONTO:
             confirmation = input("Está pronto para jogar? (s/n) ")
             if confirmation.lower() == "s":
                 print("Vamos jogar!")
-                server_socket.send("pronto".encode())
+                server_socket.send(pickle.dumps(Msg(TipoMsg.PRONTO, True)))
+                pronto = True
             else:
                 print("Talvez na próxima")
-        else:
-            print(msg.decode())
-
+        elif msg.tipo == TipoMsg.MSGSIMPLES:
+            print(msg.conteudo)
+        elif msg.tipo == TipoMsg.CARTAS:
+            print(len(msg.conteudo))
+            mao = msg.conteudo
+            for c in mao: print(c)
 
 start()
